37576311efd083dbf4f39710c692fbb9be31463d
Brian Nguyen <nguyen.brian70@gmail.com>
2021-01-17 22:55:00 -0500

    Add basic shoppies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
+require("dotenv").config();
+
 module.exports = {
   siteMetadata: {
     title: "shopify-frontend-shoppies",
   },
-  plugins: ["gatsby-plugin-styled-components"],
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-plugin-web-font-loader",
+      options: {
+        typekit: {
+          id: process.env.TYPEKIT_ID,
+        },
+      },
+    },
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,150 +1,209 @@
-import * as React from "react"
+import React, { useEffect, useState, useLoca } from "react";
+import styled from "styled-components";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: "300",
-  fontSize: "24px",
-  maxWidth: "560px",
+const apiKey = "e8ecfb7c";
+
+const Main = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: futura-pt, sans-serif;
+  font-weight: 500;
+  font-style: normal;
+`;
+
+const Container = styled.div`
+  width: 80%;
+  padding: 10px;
+`;
+
+const SearchContainer = styled.div`
+  border: 1px solid black;
+`;
+
+const SearchInput = styled.input`
+  width: 100%;
+  height: 40px;
+  padding: 0;
+  border: 1px solid black;
+`;
+
+const ResultsContainer = styled.div`
+  border: 1px solid black;
+  padding: 10px;
+  max-height: 500px;
+  overflow-y: scroll;
+  display: flex;
+`;
+
+const NominationsContainer = styled.div`
+  border: 1px solid black;
+  padding: 10px;
+`;
+
+const NominationsFlex = styled.div`
+  display: flex;
+`;
+
+const MovieDiv = styled.div`
+  /* height: 0px; */
+  width: 300px;
+  max-height: 450px;
+  border: 1px solid black;
+  padding: 10px;
+  margin: 10px;
+`;
+
+const MovieImg = styled.img`
+  height: 200px;
+`;
+
+const NominateButton = styled.button`
+  display: block;
+`;
+
+const Banner = styled.div`
+  background-color: black;
+  color: white;
+  font-size: 30px;
+  padding: 10px;
+`;
+// Hook sourced from usehooks.com
+function useLocalStorage(key, initialValue) {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value) => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
 }
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: "16px",
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: "14px",
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#000000",
-  },
-]
-
-// markup
 const IndexPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-  )
-}
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [nominations, setNominations] = useLocalStorage("nominations", []);
+  const [nominationIDs, setNominationIDs] = useLocalStorage(
+    "nominationIDs",
+    []
+  );
 
-export default IndexPage
+  const fetchData = (title) => {
+    fetch(`http://www.omdbapi.com/?s=${title}&apikey=${apiKey}&type=movie`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.Response == "True") {
+          setMovies((prev) => [...data.Search]);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const addNomination = (movie) => {
+    if (!nominationIDs.includes(movie.imdbID)) {
+      setNominations((prev) => [...prev, movie]);
+      setNominationIDs((prev) => [...prev, movie.imdbID]);
+    }
+  };
+
+  const removeNomination = (movie) => {
+    let index = nominationIDs.findIndex((movieId) => movie.imdbID);
+    if (index != -1) {
+      setNominations((prev) => [
+        ...prev.slice(0, index),
+        ...prev.slice(index + 1),
+      ]);
+      setNominationIDs((prev) => [
+        ...prev.slice(0, index),
+        ...prev.slice(index + 1),
+      ]);
+    }
+  };
+
+  const searchHandler = (event) => {
+    setSearchTerm(event.target.value);
+    fetchData(event.target.value);
+  };
+  return (
+    <Main>
+      <Container>
+        <title>The Shoppies Nominations</title>
+        <h1>The Shoppies Nominations</h1>
+        {nominations.length === 5 ? (
+          <Banner>Congrats! You've selected your five nominations!</Banner>
+        ) : (
+          ""
+        )}
+        {nominations.length > 5 ? (
+          <Banner>Error! You've selected more than five nominations!</Banner>
+        ) : (
+          ""
+        )}
+        <SearchInput
+          placeholder="Search for movie titles..."
+          value={searchTerm}
+          onChange={searchHandler}
+        ></SearchInput>
+        <ResultsContainer>
+          {movies.map((movie) => (
+            <MovieDiv>
+              <h3>{movie.Title}</h3>
+              <h3>{movie.Year}</h3>
+              {movie.Poster !== "N/A" ? <MovieImg src={movie.Poster} /> : <></>}
+              {nominationIDs.includes(movie.imdbID) ? (
+                <NominateButton disabled={true}>+ Nominate </NominateButton>
+              ) : (
+                <NominateButton
+                  onClick={() => {
+                    addNomination(movie);
+                  }}
+                >
+                  + Nominate{" "}
+                </NominateButton>
+              )}
+            </MovieDiv>
+          ))}
+        </ResultsContainer>
+        <NominationsContainer>
+          <h1>Your Nominations ({nominations.length}/5)</h1>
+          <NominationsFlex>
+            {nominations.map((movie) => (
+              <MovieDiv>
+                <h3>{movie.Title}</h3>
+                <h3>{movie.Year}</h3>
+                <MovieImg src={movie.Poster} />
+                <NominateButton
+                  onClick={() => {
+                    removeNomination(movie);
+                  }}
+                >
+                  X
+                </NominateButton>
+              </MovieDiv>
+            ))}
+          </NominationsFlex>
+        </NominationsContainer>
+      </Container>
+    </Main>
+  );
+};
+
+export default IndexPage;
